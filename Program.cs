0ea8de48d9839fc257fb4eb1c71458daf96ea20b
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-03-07 00:41:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Dersleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Hello World");
+            //Console.Write("Benim adım İrem Özdemir");
+
+            //Console.WriteLine("Kişi tanıtım kartı");
+            //Console.WriteLine();
+            //Console.WriteLine("Adınız: İrem");
+            //Console.WriteLine("Soyadınız: Özdemir");
+            //Console.WriteLine("Mesleğiniz: Öğrenci");
+
+            //Değişkenler
+            //Bellek --> Ram Geçici Bellek
+            //Sayısal,Alfabetik,Lojik Veriler,Karakter...
+
+            //string
+
+            //Değişken_Türü  Değişken_ismi
+
+            //string city;
+            //city = "Dublin";
+            //Console.Write(city);
+
+            //string capital, country;
+            //capital = "Ankara";
+            //country = "Türkiye";
+            //Console.Write(capital + " " + country);
+
+            //camel Case
+            //string nameSurname;
+            //nameSurname = "İrem Özdemir";
+            //Console.Write(nameSurname);
+
+            //string job;
+            //job = "Software Engineer";
+            //Console.Write("Meslek:" + job);
+
+            //int değişkenler
+
+            //int number;
+            //number = 20;
+            //Console.Write(number);
+
+            //int n1, n2, total;
+            //n1 = 40;
+            //n2 = 85;
+            //total = n1 + n2;
+            //Console.Write(total);
+
+            //int number1, number2, addition, extraction, multiplication, division;
+            //number1 = 20;
+            //number2 = 5;
+            //addition = number1 + number2;
+            //extraction = number1 - number2;
+            //multiplication = number1 * number2;
+            //division = number1 / number2;
+            //Console.WriteLine("Toplam: " + addition);
+            //Console.WriteLine("Fark: " + extraction);
+            //Console.WriteLine("Çarpım: " + multiplication);
+            //Console.WriteLine("Bölüm: " + division);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
