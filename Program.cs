46f8441868083411be970f28ef673ee87d572a15
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-03-11 22:52:30 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Console_Dersleri.Model;
 
 namespace Console_Dersleri
 {
@@ -14,125 +15,48 @@ namespace Console_Dersleri
     {
         static void Main(string[] args)
         {
-            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-HJ161GJ\\SQLEXPRESS;Initial Catalog=DbVisit;Integrated Security=True;Encrypt=False");
+            //adonet vs entity framework
+            //code first vs dbfirst
+            //ASP.NET CORE
+            //DbFirst
 
-            void Listele()
-            {
-                connection.Open();
-                SqlCommand command = new SqlCommand("Select * from TblPerson", connection);
-                //DataTable dt = new DataTable();
-                SqlDataReader reader = command.ExecuteReader();
-                while (reader.Read())
+            DbVisitEntities db = new DbVisitEntities();
+            void listele()
+            { 
+                var values = db.TblCountry.ToList();
+                foreach (var item in values)
                 {
-                    Console.WriteLine(reader[0] + " | " + reader[1] + " | " + reader[2]);
-                    Console.WriteLine("-------------------------------------------");
+                Console.WriteLine(item.CountryId + "-" + item.CountryName + "-" + item.CountryCapital);
+                Console.WriteLine("-------------------------------------------------");
                 }
-                connection.Close();
             }
+            //--Sınıf--Prop--Value
 
-                //Ekleme İşlemi
+            //TblPerson t = new TblPerson();
+            //t.City = "İzmir";
+            //t.Name = "Berat Yıldırım";
+            //db.TblPerson.Add(t);
+            //db.SaveChanges();
 
-                //connection.Open();
-                //SqlCommand command2 = new SqlCommand("Insert into TblPerson (Name, City) values (@p1,@p2)", connection);
-                //command2.Parameters.AddWithValue("@p1", "Mesut Kaya");
-                //command2.Parameters.AddWithValue("@p2", "Elazığ");
-                //command2.ExecuteNonQuery();
-                //connection.Close();
 
-                //Silme İşlemi
+            //TblCountry t= new TblCountry();
+            //t.CountryName = "İspanya";
+            //t.CountryCapital = "Madrid";
+            //db.TblCountry.Add(t);
+            //db.SaveChanges();
 
-                //connection.Open();
-                //SqlCommand command3 = new SqlCommand("Delete from TblPerson where Id=@p1", connection);
-                //command3.Parameters.AddWithValue("@p1", 8);
-                //command3.ExecuteNonQuery();
-                //connection.Close();
+            //Console.WriteLine("Silinecek değeri girin:");
+            //int countryCode = Convert.ToInt32(Console.ReadLine());
 
-                //Güncelleme İşlemi
-                //connection.Open();
-                //SqlCommand command4 = new SqlCommand("Update TblPerson set Name=@p1, City=@p2 where Id=@p3", connection);
-                //command4.Parameters.AddWithValue("@p1", "Veli Bulut");
-                //command4.Parameters.AddWithValue("@p2", "Kayseri");
-                //command4.Parameters.AddWithValue("@p3", 1);
-                //command4.ExecuteNonQuery();
-                //connection.Close();
 
-                //string name, city;
-                //Console.WriteLine("****************");
-                //Console.WriteLine();
-                //Console.Write("Kişi Adı:");
-                //name = Console.ReadLine();
-                //Console.WriteLine();
-                //Console.Write("Şehir:");
-                //city = Console.ReadLine();
-                //Console.WriteLine();
-                //Console.WriteLine("Kişi sisteme başarılı bir şekilde eklendi.");
-                //Console.WriteLine();
-                //Console.WriteLine("****************");
+            //var value=db.TblCountry.Find(countryCode);
+            //db.TblCountry.Remove(value);
+            //db.SaveChanges();
 
-                //connection.Open();
-                //SqlCommand command5 = new SqlCommand("Insert into TblPerson (Name, City) values (@p1,@p2)", connection);
-                //command5.Parameters.AddWithValue("@p1", name);
-                //command5.Parameters.AddWithValue("@p2", city);
-                //command5.ExecuteNonQuery();
-                //connection.Close();
-
-                //Listele();
-
-                //Arama İşlemi
-                //Console.WriteLine("*************************");
-                //Console.WriteLine();
-                //Console.Write("Aranacak şehri giriniz:");
-                //string city = Console.ReadLine();
-                //Console.WriteLine();
-                //Console.WriteLine("*************************");
-                //Console.WriteLine();
-                //connection.Open();
-                //SqlCommand command6 = new SqlCommand("Select * from TblPerson where City=@p1", connection);
-                //command6.Parameters.AddWithValue("@p1", city);
-                //SqlDataReader reader2=command6.ExecuteReader();
-                //while (reader2.Read())
-                //{
-                //    Console.WriteLine(reader2[0] + " | " + reader2[1] + " | " + reader2[2]);
-                //    Console.WriteLine("-------------------------------------------");
-                //}
-
-                //connection.Close();
-
-                //connection.Open();
-                //SqlCommand command7 = new SqlCommand("Select count(*) from TblPerson", connection);
-                //SqlDataReader reader3 = command7.ExecuteReader();
-                //while (reader3.Read())
-                //{
-                //    Console.WriteLine("Toplam Personel Sayısı: " + reader3[0]);
-                //}
-                //connection.Close();
-
-                //connection.Open();
-
-                //SqlCommand command8 = new SqlCommand("Select Avg(Price) from TblFlight", connection);
-                //SqlDataReader reader4 = command8.ExecuteReader();
-                //while (reader4.Read())
-                //{
-                //    Console.WriteLine("Ortalama Uçuş Fiyatı : " + reader4[0]);
-                //}
-
-                //connection.Close();
-
-            //connection.Open();
-            //SqlCommand command9 = new SqlCommand("Select City, Count(*) from TblPerson Group By City ", connection);
-            //SqlDataReader reader5 = command9.ExecuteReader();
-            //while (reader5.Read())
-            //{
-            //    Console.WriteLine(reader5[0] + " | " + reader5[1]);
-            //    Console.WriteLine("-------------------------------------------");
-            //}
-
-            //connection.Close();
-
+            //listele();
 
             Console.ReadLine();
-                }
-            }
         }
+    }
+}
 
-//Data Source=DESKTOP-HJ161GJ\SQLEXPRESS;Initial Catalog=DbVisit;Integrated Security=True;Encrypt=False
